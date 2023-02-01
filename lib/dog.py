902e6ea3e1ecd2bbf14770a3249d43222d017e60
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-02-01 15:10:33 -0500

    passed tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,27 +1,33 @@
 from sqlalchemy import create_engine
+from models import Dog
 
 engine = create_engine('sqlite:///:memory:')
 
 def create_table(base):
-    pass
+    base.metadata.create_all(engine)
+    return engine
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
 
-def new_from_db(session):
-    pass
+def new_from_db(session, dog):
+    return session.query(Dog).filter(Dog.id == dog.id).first()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    return session.query(Dog).filter(Dog.id == dog.id).update({
+        Dog.breed: breed
+    })
